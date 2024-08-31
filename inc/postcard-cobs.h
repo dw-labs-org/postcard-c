90afd225addfb5914f2ea3aef9b0c355d054fa6c
dom <domwilson95@hotmail.co.uk>
2024-08-31 18:20:15 +0100

    test: signed integer passing

--- inc/postcard-cobs.h
+++ inc/postcard-cobs.h
@@ -14,5 +14,7 @@ void postcard_cobs_encode_i8(struct cobs *cobs, int8_t value);
 
 void postcard_cobs_encode_i16(struct cobs *cobs, int16_t value);
 
+void postcard_cobs_encode_i32(struct cobs *cobs, int32_t value);
+
 void postcard_cobs_encode_float(struct cobs *cobs, float value);
 #endif
--- test/postcard_cobs_test.c
+++ test/postcard_cobs_test.c
@@ -162,10 +162,198 @@ void u32_encoding(void) {
   cobs_reset(&cobs);
 }
 
+void i16_encoding(void) {
+  struct cobs cobs;
+  uint8_t buf[10];
+  cobs_init(&cobs, buf, 10);
+
+  cobs_start_frame(&cobs);
+  postcard_cobs_encode_i16(&cobs, 0);
+  cobs_end_frame(&cobs);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[0]);
+  TEST_ASSERT_EQUAL_UINT8(1, buf[1]);
+  TEST_ASSERT_EQUAL_UINT8(1, buf[2]);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[3]);
+  cobs_reset(&cobs);
+
+  cobs_start_frame(&cobs);
+  postcard_cobs_encode_i16(&cobs, -1);
+  cobs_end_frame(&cobs);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[0]);
+  TEST_ASSERT_EQUAL_UINT8(2, buf[1]);
+  TEST_ASSERT_EQUAL_UINT8(1, buf[2]);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[3]);
+  cobs_reset(&cobs);
+
+  cobs_start_frame(&cobs);
+  postcard_cobs_encode_i16(&cobs, 1);
+  cobs_end_frame(&cobs);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[0]);
+  TEST_ASSERT_EQUAL_UINT8(2, buf[1]);
+  TEST_ASSERT_EQUAL_UINT8(2, buf[2]);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[3]);
+  cobs_reset(&cobs);
+
+  cobs_start_frame(&cobs);
+  postcard_cobs_encode_i16(&cobs, 63);
+  cobs_end_frame(&cobs);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[0]);
+  TEST_ASSERT_EQUAL_UINT8(2, buf[1]);
+  TEST_ASSERT_EQUAL_UINT8(0x7E, buf[2]);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[3]);
+  cobs_reset(&cobs);
+
+  cobs_start_frame(&cobs);
+  postcard_cobs_encode_i16(&cobs, -64);
+  cobs_end_frame(&cobs);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[0]);
+  TEST_ASSERT_EQUAL_UINT8(2, buf[1]);
+  TEST_ASSERT_EQUAL_UINT8(0x7F, buf[2]);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[3]);
+  cobs_reset(&cobs);
+
+  cobs_start_frame(&cobs);
+  postcard_cobs_encode_i16(&cobs, 64);
+  cobs_end_frame(&cobs);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[0]);
+  TEST_ASSERT_EQUAL_UINT8(3, buf[1]);
+  TEST_ASSERT_EQUAL_UINT8(0x80, buf[2]);
+  TEST_ASSERT_EQUAL_UINT8(0x01, buf[3]);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[4]);
+  cobs_reset(&cobs);
+
+  cobs_start_frame(&cobs);
+  postcard_cobs_encode_i16(&cobs, -65);
+  cobs_end_frame(&cobs);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[0]);
+  TEST_ASSERT_EQUAL_UINT8(3, buf[1]);
+  TEST_ASSERT_EQUAL_UINT8(0x81, buf[2]);
+  TEST_ASSERT_EQUAL_UINT8(0x01, buf[3]);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[4]);
+  cobs_reset(&cobs);
+
+  cobs_start_frame(&cobs);
+  postcard_cobs_encode_i16(&cobs, 32767);
+  cobs_end_frame(&cobs);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[0]);
+  TEST_ASSERT_EQUAL_UINT8(4, buf[1]);
+  TEST_ASSERT_EQUAL_UINT8(0xFE, buf[2]);
+  TEST_ASSERT_EQUAL_UINT8(0xFF, buf[3]);
+  TEST_ASSERT_EQUAL_UINT8(0x03, buf[4]);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[5]);
+  cobs_reset(&cobs);
+
+  cobs_start_frame(&cobs);
+  postcard_cobs_encode_i16(&cobs, -32768);
+  cobs_end_frame(&cobs);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[0]);
+  TEST_ASSERT_EQUAL_UINT8(4, buf[1]);
+  TEST_ASSERT_EQUAL_UINT8(0xFF, buf[2]);
+  TEST_ASSERT_EQUAL_UINT8(0xFF, buf[3]);
+  TEST_ASSERT_EQUAL_UINT8(0x03, buf[4]);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[5]);
+  cobs_reset(&cobs);
+}
+
+void i32_encoding(void) {
+  struct cobs cobs;
+  uint8_t buf[10];
+  cobs_init(&cobs, buf, 10);
+
+  cobs_start_frame(&cobs);
+  postcard_cobs_encode_i32(&cobs, 0);
+  cobs_end_frame(&cobs);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[0]);
+  TEST_ASSERT_EQUAL_UINT8(1, buf[1]);
+  TEST_ASSERT_EQUAL_UINT8(1, buf[2]);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[3]);
+  cobs_reset(&cobs);
+
+  cobs_start_frame(&cobs);
+  postcard_cobs_encode_i32(&cobs, -1);
+  cobs_end_frame(&cobs);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[0]);
+  TEST_ASSERT_EQUAL_UINT8(2, buf[1]);
+  TEST_ASSERT_EQUAL_UINT8(1, buf[2]);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[3]);
+  cobs_reset(&cobs);
+
+  cobs_start_frame(&cobs);
+  postcard_cobs_encode_i32(&cobs, 1);
+  cobs_end_frame(&cobs);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[0]);
+  TEST_ASSERT_EQUAL_UINT8(2, buf[1]);
+  TEST_ASSERT_EQUAL_UINT8(2, buf[2]);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[3]);
+  cobs_reset(&cobs);
+
+  cobs_start_frame(&cobs);
+  postcard_cobs_encode_i32(&cobs, 63);
+  cobs_end_frame(&cobs);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[0]);
+  TEST_ASSERT_EQUAL_UINT8(2, buf[1]);
+  TEST_ASSERT_EQUAL_UINT8(0x7E, buf[2]);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[3]);
+  cobs_reset(&cobs);
+
+  cobs_start_frame(&cobs);
+  postcard_cobs_encode_i32(&cobs, -64);
+  cobs_end_frame(&cobs);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[0]);
+  TEST_ASSERT_EQUAL_UINT8(2, buf[1]);
+  TEST_ASSERT_EQUAL_UINT8(0x7F, buf[2]);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[3]);
+  cobs_reset(&cobs);
+
+  cobs_start_frame(&cobs);
+  postcard_cobs_encode_i32(&cobs, 64);
+  cobs_end_frame(&cobs);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[0]);
+  TEST_ASSERT_EQUAL_UINT8(3, buf[1]);
+  TEST_ASSERT_EQUAL_UINT8(0x80, buf[2]);
+  TEST_ASSERT_EQUAL_UINT8(0x01, buf[3]);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[4]);
+  cobs_reset(&cobs);
+
+  cobs_start_frame(&cobs);
+  postcard_cobs_encode_i32(&cobs, -65);
+  cobs_end_frame(&cobs);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[0]);
+  TEST_ASSERT_EQUAL_UINT8(3, buf[1]);
+  TEST_ASSERT_EQUAL_UINT8(0x81, buf[2]);
+  TEST_ASSERT_EQUAL_UINT8(0x01, buf[3]);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[4]);
+  cobs_reset(&cobs);
+
+  cobs_start_frame(&cobs);
+  postcard_cobs_encode_i32(&cobs, 32767);
+  cobs_end_frame(&cobs);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[0]);
+  TEST_ASSERT_EQUAL_UINT8(4, buf[1]);
+  TEST_ASSERT_EQUAL_UINT8(0xFE, buf[2]);
+  TEST_ASSERT_EQUAL_UINT8(0xFF, buf[3]);
+  TEST_ASSERT_EQUAL_UINT8(0x03, buf[4]);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[5]);
+  cobs_reset(&cobs);
+
+  cobs_start_frame(&cobs);
+  postcard_cobs_encode_i32(&cobs, -32768);
+  cobs_end_frame(&cobs);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[0]);
+  TEST_ASSERT_EQUAL_UINT8(4, buf[1]);
+  TEST_ASSERT_EQUAL_UINT8(0xFF, buf[2]);
+  TEST_ASSERT_EQUAL_UINT8(0xFF, buf[3]);
+  TEST_ASSERT_EQUAL_UINT8(0x03, buf[4]);
+  TEST_ASSERT_EQUAL_UINT8(0, buf[5]);
+  cobs_reset(&cobs);
+}
+
 // not needed when using generate_test_runner.rb
 int main(void) {
   UNITY_BEGIN();
   RUN_TEST(u16_encoding);
   RUN_TEST(u32_encoding);
+  RUN_TEST(i16_encoding);
+  RUN_TEST(i32_encoding);
   return UNITY_END();
 }
